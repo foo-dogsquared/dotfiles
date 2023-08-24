84a7e6d1e365df746ac396d71d377b96a6d938f7
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-08-24 10:30:58 +0800

    emacs: move config to custom notebook config
    Which is in another Git repo.

--- emacs/config.el
+++ emacs/config.el
@@ -10,10 +10,6 @@
 (setq doom-theme 'doom-material-dark)
 
 (setq org-directory "~/library/writings/wiki"
-      org-roam-directory "~/library/writings/wiki"
-      org-roam-db-location (f-join org-roam-directory "org-roam.db")
-      org-agenda-files '("~/library/writings/wiki/inbox")
-      org-roam-dailies-directory (f-join org-roam-directory "daily")
       org-id-link-to-org-use-id t
       +file-templates-dir (expand-file-name "templates" doom-private-dir)
       +wiki-directory "~/library/writings/wiki"
@@ -36,20 +32,6 @@
 
 (add-to-list 'org-modules 'org-habit)
 (add-to-list 'org-modules 'org-checklist)
-
-(defvar my/wiki-asset-directory-name "assets")
-(defvar my/wiki-exercises-directory "challenges")
-
-(defun my/create-assets-folder ()
-  "A quick convenient function to create an assets folder in the wiki folder."
-  (interactive)
-  (if (and (not (string= (f-base (buffer-file-name))
-                         my/wiki-asset-directory-name))
-           (f-descendant-of-p (buffer-file-name)
-                              (expand-file-name +wiki-directory)))
-      (f-mkdir my/wiki-asset-directory-name
-               (f-join my/wiki-asset-directory-name (file-name-sans-extension (buffer-file-name))))
-    (message "Not in the wiki directory.")))
 
 (after! tex
   (TeX-engine-set "luatex")
@@ -74,27 +56,6 @@
                             "* TODO [[%:link][%:description]]\n%x"
                             :immediate-finish t))))
 
-
-;; Custom keybindings
-(map!
- (:when (modulep! :tools wiki)
-  :leader
-  :prefix "nr" :desc "Create the asset folder" "m" #'my/create-assets-folder)
-
- (:when (modulep! :editor format)
-  :n "g=" #'+format/buffer))
-
-;; A workaround for electric-indent plugin.
-;; See https://github.com/hlissner/doom-emacs/issues/3172 for more details.
-(add-hook 'org-mode (lambda ()
-                      (electric-indent-local-mode -1)))
-
-;; Automate updating timestamps on save.
-(add-hook! 'before-save-hook 'time-stamp)
-
 ;; Load a custom configuration for muh wiki.
-(add-hook! 'counsel-projectile-mode-hook (lambda ()
-                                           (message (file-name-directory (buffer-file-name)))))
 (load-file (f-join +wiki-directory "config.el"))
-
 ;;; config.el ends here
