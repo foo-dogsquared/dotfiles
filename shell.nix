5e116e1681cbb7ea7065c9bc16af69cb5be845d5
Gabriel Arazas <foodogsquared@foodogsquared.one>
2023-10-31 14:38:20 +0800

    shell.nix: remove pinned nixpkgs
    It is already handled with flakes anyways.

--- shell.nix
+++ shell.nix
@@ -1,14 +1,3 @@
-let
-  # See https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs for more information on pinning
-  nixpkgs = builtins.fetchTarball {
-    # Descriptive name to make the store path easier to identify
-    name = "nixpkgs-unstable-2020-11-07";
-    # Commit hash for nixos-unstable as of 2019-02-26
-    url = https://github.com/NixOS/nixpkgs/archive/c54c614000644ecf9b8f8e9c873cfa91d1c05bf1.tar.gz;
-    # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "19xmsj1dhq25arhsfx0sl3r1y0zgpzfwhybc5dsxr1szh71wz3xs";
-  };
-in
 { pkgs ? import <nixpkgs> {} }:
 
 with pkgs;
